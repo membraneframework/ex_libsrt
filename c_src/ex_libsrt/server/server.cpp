c3ac74071db0993cb23c5bbad040513408d8a864
Liam Stupid Name Humphreys <liam.humphreys1989@gmail.com>
2026-02-10 23:21:14 +0000

    Update server.cpp

--- c_src/ex_libsrt/server/server.cpp
+++ c_src/ex_libsrt/server/server.cpp
@@ -1,6 +1,7 @@
 #include "server.h"
 
 #include <chrono>
+#include <cstring>
 #include <exception>
 #include <string>
 #include <vector>
@@ -12,17 +13,29 @@ void Server::Run(const std::string& address,
                  int latency_ms) {
   this->password = password;
   this->latency_ms = latency_ms;
-  
+
   srt_sock = srt_create_socket();
   if (srt_sock == SRT_ERROR) {
     throw std::runtime_error(std::string(srt_getlasterror_str()));
   }
 
-  struct sockaddr_in sa;
-  sa.sin_family = AF_INET;
-  sa.sin_port = htons(port);
-  if (inet_pton(AF_INET, address.c_str(), &(sa).sin_addr) != 1) {
-    throw std::runtime_error("Failed to parse server address");
+  struct sockaddr_storage ss;
+  socklen_t ss_len;
+  memset(&ss, 0, sizeof(ss));
+
+  struct sockaddr_in6 *sa6 = reinterpret_cast<struct sockaddr_in6*>(&ss);
+  struct sockaddr_in  *sa4 = reinterpret_cast<struct sockaddr_in*>(&ss);
+
+  if (inet_pton(AF_INET6, address.c_str(), &sa6->sin6_addr) == 1) {
+    sa6->sin6_family = AF_INET6;
+    sa6->sin6_port = htons(port);
+    ss_len = sizeof(struct sockaddr_in6);
+  } else if (inet_pton(AF_INET, address.c_str(), &sa4->sin_addr) == 1) {
+    sa4->sin_family = AF_INET;
+    sa4->sin_port = htons(port);
+    ss_len = sizeof(struct sockaddr_in);
+  } else {
+    throw std::runtime_error("Failed to parse server address: " + address);
   }
 
   int yes = 1;
@@ -35,7 +48,7 @@ void Server::Run(const std::string& address,
     }
   }
 
-  srt_bind_sock = srt_bind(srt_sock, (struct sockaddr*)&(sa), sizeof sa);
+  srt_bind_sock = srt_bind(srt_sock, reinterpret_cast<struct sockaddr*>(&ss), ss_len);
   if (srt_bind_sock == SRT_ERROR) {
     throw std::runtime_error(std::string(srt_getlasterror_str()));
   }
