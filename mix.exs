56709f96ee0396f4e37a0d8beb322019f599736d
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2026-04-15 16:13:42 +0200

    Update ex_doc, add llms.txt support, update description

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ExLibSRT.MixProject do
   use Mix.Project
 
-  @version "0.1.7"
+  @version "0.1.8"
   @github_url "https://github.com/membraneframework/ex_libsrt"
 
   def project do
@@ -14,13 +14,14 @@ defmodule ExLibSRT.MixProject do
       compilers: [:unifex, :bundlex] ++ Mix.compilers(),
       deps: deps(),
       # hex
-      description: "SRT bindings for Elixir",
+      description: "SRT protocol bindings for low-latency streaming.",
       package: package(),
       # docs
       name: "ExLibSRT",
       source_url: @github_url,
       docs: docs(),
-      homepage_url: "https://membrane.stream"
+      homepage_url: "https://membrane.stream",
+      aliases: [docs: ["docs", &prepend_llms_links/1]]
     ]
   end
 
@@ -44,7 +45,6 @@ defmodule ExLibSRT.MixProject do
     [
       main: "readme",
       extras: ["README.md", "LICENSE"],
-      formatters: ["html"],
       source_ref: "v#{@version}",
       nest_modules_by_prefix: [ExLibSRT]
     ]
@@ -62,7 +62,22 @@ defmodule ExLibSRT.MixProject do
       {:membrane_precompiled_dependency_provider, "~> 0.2.0"},
       {:credo, "~> 1.4", only: :dev, runtime: false},
       {:dialyxir, "~> 1.1", only: :dev, runtime: false},
-      {:ex_doc, "~> 0.31", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.40", only: :dev, runtime: false}
     ]
   end
+
+defp prepend_llms_links(_) do
+  path = "doc/llms.txt"
+
+  if File.exists?(path) do
+    existing = File.read!(path)
+
+    header =
+      "- [Membrane Core AI Skill](https://hexdocs.pm/membrane_core/skill.md)\n" <>
+        "- [Membrane Core](https://hexdocs.pm/membrane_core/llms.txt)\n\n"
+
+    File.write!(path, header <> existing)
+  end
+end
+
 end
