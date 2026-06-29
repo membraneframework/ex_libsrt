f0102f5a68ea876b27798699f0df7671dee8b67a
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2026-06-29 11:25:39 +0200

    Set ex_doc dependency to >= 0.40.0

--- mix.exs
+++ mix.exs
@@ -85,7 +85,7 @@ defmodule ExLibSRT.MixProject do
       {:membrane_precompiled_dependency_provider, "~> 0.2.0"},
       {:credo, ">= 0.0.0", only: :dev, runtime: false},
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
-      {:ex_doc, "~> 0.40", only: :dev, runtime: false}
+      {:ex_doc, ">= 0.40.0", only: :dev, runtime: false}
     ]
   end
 
